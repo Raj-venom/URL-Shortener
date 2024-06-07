34beb2f50dc17491d0dad370d9ee8e7b9d9e02d1
Raj-venom <rajsingh.rsr9824@gmail.com>
2024-06-07 15:16:53 +0545

    Add utils files

---
+++ Backend/src/models/url.model.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const urlSchema = new mongoose.Schema(
+    {
+        redirectURL: {
+            type: String,
+            required: true
+        },
+        shortId: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        visitHistory: [{ timestamps: { type: Number } }]
+    },
+    { timestamps: true }
+)
+
+export const Url = mongoose.model("Url", urlSchema)
