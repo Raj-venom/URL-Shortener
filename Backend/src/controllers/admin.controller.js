660609fdaca9d200c5b53d19d08324cc05f4d299
Raj-venom <rajsingh.rsr9824@gmail.com>
2024-06-13 14:54:04 +0545

    Add Admin endpoint to getAll Urls

--- Backend/src/controllers/admin.controller.js
+++ Backend/src/controllers/admin.controller.js
@@ -1,4 +1,3 @@
-import mongoose from "mongoose";
 import { asyncHandler } from "../utils/assyncHandler.js"
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
